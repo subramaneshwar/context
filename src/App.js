66b1a4ad6714ce99fcd58d0c7c3ebe609b2b11a0
subramaneshwar <subramaneshwar@gmail.com>
2023-04-05 16:48:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Acomp from './Components/Acomp';
+
+function App() {
+  return (
+    <div className="App">
+      <Acomp/>
+    </div>
+  );
+}
+
+export default App;
